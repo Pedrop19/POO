d39127785b09fb56a00c9e87a2d842d2e2a8777a
Pedro Rafael Lázaro Nevado <119618710+Pedrop19@users.noreply.github.com>
2023-02-28 17:08:03 +0100

    28/02/23

--- RestauranteGrupo/src/main/java/com/mycompany/restaurantegrupo/RestauranteGrupo.java
+++ RestauranteGrupo/src/main/java/com/mycompany/restaurantegrupo/RestauranteGrupo.java
@@ -14,17 +14,16 @@ public class RestauranteGrupo {
     public static void main(String[] args) {
         int opc = 0;
         do{
-            System.out.println("1. CREAR PLATO");
-            System.out.println("2. AÑADIR EMPLEADO");
-            System.out.println("3. DESPEDIR EMPLEADO");
-            System.out.println("4. BORRAR UN PLATO");
-            System.out.println("5. AÑADIR INGREDIENTE");
-            System.out.println("6. RECEPCIONAR PEDIDO DE PROVEEDOR");
-            System.out.println("7. AGREGAR PROVEEDOR");
-            System.out.println("8. MOSTRAR BENEFICIO");
-            System.out.println("9. MOSTRAR AVISO DEL ALMACEN");
-            System.out.println("10. VENTA DE PLATO");
-            System.out.println("11. SALIR");
-        }while(opc !=11);
+            System.out.println("1. AÑADIR EMPLEADO");
+            System.out.println("2. RECEPCIONAR PEDIDO DE PROVEEDOR");
+            System.out.println("3. CREAR PLATO");
+            System.out.println("4. DESPEDIR EMPLEADO");
+            System.out.println("5. BORRAR UN PLATO");
+            System.out.println("6. AGREGAR PROVEEDOR");
+            System.out.println("7. MOSTRAR BENEFICIO");
+            System.out.println("8. MOSTRAR AVISO DEL ALMACEN");
+            System.out.println("9. VENTA DE PLATO");
+            System.out.println("10. SALIR");
+        }while(opc !=10);
     }
 }
