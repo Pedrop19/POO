b061dfe2d09d2dced9dcf5fb9a301b5e47899007
Pedro Rafael Lázaro Nevado <119618710+Pedrop19@users.noreply.github.com>
2023-03-07 20:14:14 +0100

    07/03/23

--- Formas/src/Clases/Circulo.java
+++ Formas/src/Clases/Circulo.java
@@ -5,5 +5,4 @@ public class Circulo extends Elipse {
 	public Circulo(String color, String nombre, Punto centro, double radio_mayor, double radio_menor) {
 		super(color, nombre, centro, radio_mayor, radio_menor);
 	}
-
 }
