57e076185e504c744a738bae0ee88e09cd72bb6b
Pedro Rafael Lázaro Nevado <119618710+Pedrop19@users.noreply.github.com>
2023-03-09 17:48:13 +0100

    09/03/23

--- Facultad/src/Enums/Est_Civil.java
+++
@@ -1,5 +0,0 @@
-package Facultad.src.Enums;
-
-public enum Est_Civil {
-    SOLTERO, CASADO, VIUDO, SEPARADO, DIVORCIADO
-}
--- Facultad/src/Ppal/Ppal.java
+++
@@ -1,7 +0,0 @@
-package Facultad.src.Ppal;
-
-public class Ppal {
-    public static void main(String[] args) {
-
-    }
-}
